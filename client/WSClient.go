031d9405adfcced4d2a87899702e87c1208f02f0
wqq <wqq.master@gmail.com>
2023-12-21 16:36:29 +0800

    1. 增加底层多协议支持

--- client/WSClient.go
+++ client/WSClient.go
@@ -3,9 +3,9 @@ package client
 import (
 	"errors"
 	"github.com/go-netty/go-netty"
-	"github.com/go-netty/go-netty-transport/websocket"
 	"github.com/go-netty/go-netty/codec/format"
 	"github.com/go-netty/go-netty/codec/frame"
+	"github.com/go-netty/go-netty/transport"
 	"im-sdk/handler"
 	"im-sdk/process"
 	"im-sdk/util"
@@ -22,7 +22,9 @@ func New(url string) *WSClient {
 		Url: url,
 	}
 }
-func (_self *WSClient) Startup(process process.IIMProcess) error {
+
+// Startup p 协议 tcp.New()或者 websocket.New()
+func (_self *WSClient) Startup(process process.IIMProcess, p transport.Factory) error {
 	_self.handler = handler.NewClientHandler(process)
 	client := func(channel netty.Channel) {
 		channel.Pipeline().
@@ -30,7 +32,7 @@ func (_self *WSClient) Startup(process process.IIMProcess) error {
 			AddLast(format.JSONCodec(true, false)).
 			AddLast(_self.handler)
 	}
-	var bootstrap = netty.NewBootstrap(netty.WithClientInitializer(client), netty.WithTransport(websocket.New()))
+	var bootstrap = netty.NewBootstrap(netty.WithClientInitializer(client), netty.WithTransport(p))
 	channel, err := bootstrap.Connect(_self.Url)
 	_self.Channel = channel
 	if err != nil {
@@ -46,7 +48,7 @@ func (_self *WSClient) Startup(process process.IIMProcess) error {
 	}()
 	return nil
 }
-func (_self *WSClient) Reconnect() error {
+func (_self *WSClient) Reconnect(p transport.Factory) error {
 	//如果通道在线 先关闭
 	if _self.Channel != nil && _self.Channel.IsActive() {
 		_self.Channel.Close(errors.New("【IM】IM客户端正常关闭"))
@@ -56,7 +58,7 @@ func (_self *WSClient) Reconnect() error {
 	//停止Qos
 	_self.handler.GetMessageManager().StopQos()
 	//再重新启动
-	return _self.Startup(_self.handler.GetMessageManager().LogicProcess)
+	return _self.Startup(_self.handler.GetMessageManager().LogicProcess, p)
 }
 func (_self *WSClient) OpenLog() {
 	util.OpenLog()
